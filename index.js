bf63d4db4ac8cf0148563be03e70549949bfc4a0
Pedro Henrique Rodrigues <pedrohenriquerls@gmail.com>
2019-07-28 12:40:29 -0300

    Remove hardcoded config

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const Slackbot = require('slackbots');
 const axios = require('axios');
 const restify = require('restify');
+const parseGithubIssue = require('./issueParser.js');
+const config = require('./config.js');
 
+const { token } = config.slack;
 const bot = new Slackbot({
-  token: 'xoxb-707123959318-704993216373-1WRU6JsObSJQwWU4LsgCPdNT',
-  name: 'githubissuebot'
+  token: token,
+  name: 'Troublemaker'
 });
 
-bot.on('start', function() {
-  console.log('slackbot running...');
-});
+bot.on('start', () => console.log('slackbot running...'));
 bot.on('error', err => console.log(err));
 
 const server = restify.createServer();
@@ -17,38 +18,13 @@ server.get('/ping', function(req, res, next){
   res.send('pong');
   next();
 });
+
 server.post('/github/webhook', (req, res, next) => {
-  //console.log('request params', req.body);
-  const githubJson = JSON.parse(req.body.payload);
-  const issue = githubJson.issue;
-  const action = githubJson.action;
-  const { user, title, body, url, labels, number } = issue;
-  const formattedLabels = labels.map((label) => label.name).join('\n');
+  const issuePayload = JSON.parse(req.body.payload);
+  const issue = parseGithubIssue(issuePayload);
 
-  // TODO improve the body formatter https://api.slack.com/tools/block-kit-builder
-
-  if (action === 'opened') {
-    const messageParams = {
-      'slackbot': 'true',
-      "attachments": [
-        {
-          "pretext": `Issue _${action}_ by <${user.html_url}|${user.login}>`,
-          "color": "#36a64f",
-          "author_name": user.login,
-          "author_link": user.html_url,
-          "author_icon": user.avatar_url,
-          "title": `#${number} ${title}`,
-          'title_link': url,
-          "text": `${body}\n\n *Labels*\n${formattedLabels}`,
-          "mrkdwn_in": [
-            "text",
-            'pretext'
-          ]
-        }
-      ]
-    };
-
-    bot.postMessageToChannel('geral', null, messageParams);
+  if (issue.action === 'opened') {
+    bot.postMessageToChannel('geral', null, issue.message);
   }
 
   // events with labeled actions happen when the user change one label issue
